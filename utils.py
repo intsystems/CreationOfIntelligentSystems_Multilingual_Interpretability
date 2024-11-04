9934c97f770a6ba18f2d4bcbfe76e497d040c595
Wayfarer123 <nikitaokhotnikov@mail.ru>
2024-11-04 19:27:39 +0300

    refactor

--- utils.py
+++ utils.py
@@ -87,3 +87,96 @@ class Collator:
     
         return input_ids, attention_mask, labels
     
+
+def detect_domain_specific_neurons_for_layer(
+    layer,
+    eps=1e-2,
+    domain_name="eng",
+    type="mlp", # either 'mlp' or 'attn'
+    reset_impacts=False,
+    reset_dsn=False # True to find DSNs iteratively
+):  
+    assert type in ['mlp', 'attn']
+
+    if hasattr(layer, "dsn") and not reset_dsn:
+        layer_dsn = layer.dsn
+    else:
+        layer_dsn = {domain_name: {}}
+    
+
+    if type == 'mlp':
+        if layer.up_proj.impacts is not None:
+            if layer_dsn[domain_name].get("up_proj", None) is None:
+                layer_dsn[domain_name]["up_proj"] = torch.all(torch.abs(layer.up_proj.impacts) > eps, dim=0)
+            else:
+                layer_dsn[domain_name]["up_proj"] = layer_dsn[domain_name]["up_proj"] * torch.all(torch.abs(layer.up_proj.impacts) > eps, dim=0)
+            layer_dsn[domain_name]["down_proj"] = layer_dsn[domain_name]["up_proj"]
+
+            if reset_impacts:
+                layer.up_proj.impacts = None
+                layer.down_proj.impacts = None
+
+
+    elif type == 'attn':
+        if layer.q_proj.impacts is not None:
+            if layer_dsn[domain_name].get("q_proj", None) is None:
+                layer_dsn[domain_name]["q_proj"] = torch.all(torch.abs(layer.q_proj.impacts) > eps, dim=0)
+            else:
+                layer_dsn[domain_name]["q_proj"] = layer_dsn[domain_name]["q_proj"] * torch.all(torch.abs(layer.q_proj.impacts) > eps, dim=0)
+            layer_dsn[domain_name]["k_proj"] = layer_dsn[domain_name]["q_proj"]
+
+            if reset_impacts:
+                layer.q_proj.impacts = None
+                layer.k_proj.impacts = None
+
+    torch.cuda.empty_cache()
+    layer.dsn = layer_dsn
+    return layer
+
+
+def calculate_impacts(
+    model,
+    dataloader
+):
+    model.eval()
+    outputs = []
+    with torch.no_grad():
+        for batch in dataloader:
+            output = model(batch[0], batch[1])
+            outputs.append(output["last_hidden_state"].cpu())
+    outputs = torch.cat(outputs, dim=0)
+    return output
+
+
+def detect_domain_specific_neurons(
+    model,
+    dataloader=None,
+    eps=1e-2,
+    domain_name="eng", 
+    reset_impacts=False,
+    reset_dsn=True
+):
+    model.eval()
+
+    if dataloader is not None:
+        _ = calculate_impacts(model, dataloader)
+
+    for i in range(len(model.layers)):
+        detect_domain_specific_neurons_for_layer(
+            model.layers[i].mlp, 
+            eps=eps, 
+            domain_name=domain_name, 
+            type='mlp', 
+            reset_impacts=reset_impacts,
+            reset_dsn=reset_dsn
+        )
+        detect_domain_specific_neurons_for_layer(
+            model.layers[i].self_attn, 
+            eps=eps, 
+            domain_name=domain_name, 
+            type='attn', 
+            reset_impacts=reset_impacts,
+            reset_dsn=reset_dsn
+        )
+
+    return model
