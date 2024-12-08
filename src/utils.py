86fc62c8d508216a69fdcba846f51a7c218acf13
Wayfarer123 <nikitaokhotnikov@mail.ru>
2024-12-08 03:06:46 +0300

    dsn training code added

--- src/utils.py
+++ src/utils.py
@@ -9,37 +9,37 @@ def convert_to_Qwen2_ND(
     qwen
 ):
     qwen.to("cpu")
-    for i in range(len(qwen.layers)):
-        qwen.layers[i].mlp = Qwen2MLP_ND(qwen.layers[i].mlp)
-        qwen.layers[i].self_attn = Qwen2SdpaAttention_ND(qwen.layers[i].self_attn)
+    for i in range(len(qwen.model.layers)):
+        qwen.model.layers[i].mlp = Qwen2MLP_ND(qwen.model.layers[i].mlp)
+        qwen.model.layers[i].self_attn = Qwen2SdpaAttention_ND(qwen.model.layers[i].self_attn)
 
     return qwen
 
 def reset_impacts(
     qwen_nd
 ):
-    for i in range(len(qwen_nd.layers)):
-        qwen_nd.layers[i].mlp.up_proj.impacts = None
-        qwen_nd.layers[i].mlp.down_proj.impacts = None
+    for i in range(len(qwen_nd.model.layers)):
+        qwen_nd.model.layers[i].mlp.up_proj.impacts = None
+        qwen_nd.model.layers[i].mlp.down_proj.impacts = None
 
-        qwen_nd.layers[i].self_attn.q_proj.impacts = None
-        qwen_nd.layers[i].self_attn.k_proj.impacts = None
+        qwen_nd.model.layers[i].self_attn.q_proj.impacts = None
+        qwen_nd.model.layers[i].self_attn.k_proj.impacts = None
     return qwen_nd
 
 def impacts_off(
     qwen_nd
 ):
-    for i in range(len(qwen_nd.layers)):
-        qwen_nd.layers[i].mlp.calculate_impacts = False
-        qwen_nd.layers[i].self_attn.calculate_impacts = False
+    for i in range(len(qwen_nd.model.layers)):
+        qwen_nd.model.layers[i].mlp.calculate_impacts = False
+        qwen_nd.model.layers[i].self_attn.calculate_impacts = False
     return qwen_nd
 
 def impacts_on(
     qwen_nd
 ):
-    for i in range(len(qwen_nd.layers)):
-        qwen_nd.layers[i].mlp.calculate_impacts = True
-        qwen_nd.layers[i].self_attn.calculate_impacts = True
+    for i in range(len(qwen_nd.model.layers)):
+        qwen_nd.model.layers[i].mlp.calculate_impacts = True
+        qwen_nd.model.layers[i].self_attn.calculate_impacts = True
     return qwen_nd
 
 
@@ -47,10 +47,10 @@ def get_all_dsn(
     qwen_nd
 ):
     all_dsn = []
-    for i in range(len(qwen_nd.layers)):
+    for i in range(len(qwen_nd.model.layers)):
         all_dsn.append({
-            "mlp": qwen_nd.layers[i].mlp.dsn,
-            "attn": qwen_nd.layers[i].self_attn.dsn
+            "mlp": qwen_nd.model.layers[i].mlp.dsn,
+            "attn": qwen_nd.model.layers[i].self_attn.dsn
         })
 
     return all_dsn
@@ -199,9 +199,9 @@ def detect_domain_specific_neurons(
         outputs = None
 
     
-    for i in range(len(model.layers)):
+    for i in range(len(model.model.layers)):
         detect_domain_specific_neurons_for_layer(
-            model.layers[i].mlp, 
+            model.model.layers[i].mlp, 
             eps=eps, 
             domain_name=domain_name, 
             type='mlp', 
@@ -209,7 +209,7 @@ def detect_domain_specific_neurons(
             reset_dsn=reset_dsn
         )
         detect_domain_specific_neurons_for_layer(
-            model.layers[i].self_attn, 
+            model.model.layers[i].self_attn, 
             eps=eps, 
             domain_name=domain_name, 
             type='attn', 
@@ -225,7 +225,7 @@ def dsn_model_grads_to_train(model):
     for param in model.parameters():
         param.requires_grad = False
 
-    for layer in model.layers:
+    for layer in model.model.layers:
         for param in layer.mlp.parameters():
             param.requires_grad = True
         for param in layer.self_attn.q_proj.parameters():
@@ -239,17 +239,17 @@ def dsn_model_mask_gradients(
     model,
     domain="eng"
 ):
-    for i, layer in enumerate(model.layers):
+    for i, layer in enumerate(model.model.layers):
 
         if layer.mlp.dsn is not None:
-            layer.mlp.up_proj._grad = layer.mlp.up_proj._grad * (~layer.mlp.dsn[domain]["up_proj"])
-            layer.mlp.down_proj._grad = layer.mlp.down_proj._grad * (~layer.mlp.dsn[domain]["down_proj"])
+            layer.mlp.up_proj.weight._grad = layer.mlp.up_proj.weight._grad * (~layer.mlp.dsn[domain]["up_proj"])
+            layer.mlp.down_proj.weight._grad = layer.mlp.down_proj.weight._grad * (~layer.mlp.dsn[domain]["down_proj"])
         else:
             warnings.warn(f"layers[{i}].mlp domain specific neurons mask is not defined!", UserWarning)
 
         if layer.self_attn.dsn is not None:
-            layer.mlp.self_attn.q_proj._grad = layer.mlp.self_attn.q_proj._grad * (~layer.mlp.dsn[domain]["q_proj"])
-            layer.mlp.self_attn.k_proj._grad = layer.mlp.self_attn.k_proj._grad * (~layer.mlp.dsn[domain]["k_proj"])
+            layer.self_attn.q_proj.weight._grad = layer.self_attn.q_proj.weight._grad * (~layer.self_attn.dsn[domain]["q_proj"])
+            layer.self_attn.k_proj.weight._grad = layer.self_attn.k_proj.weight._grad * (~layer.self_attn.dsn[domain]["k_proj"])
         else:
             warnings.warn(f"layers[{i}].self_attn domain specific neurons mask is not defined!", UserWarning)
     
